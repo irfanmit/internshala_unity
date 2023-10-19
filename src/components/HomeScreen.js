c4d901f8bfad66f82077821ef59f84cf9ee70a43
faisalmit@2502 <faisalirfan2502@gmail.com>
2023-10-20 00:00:52 +0530

    adding last time

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,68 +1,41 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Import Link for routing
 import Select from 'react-select'; // Import the Select component
 import '../App.css';
 
 function HomeScreen() {
+  // State variables for search and filtering
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedTags, setSelectedTags] = useState([]);
-  const [selectedField, setSelectedField] = useState('');
-  const [selectedCondition, setSelectedCondition] = useState('');
-  const [numericValue, setNumericValue] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  // Available numerical fields and conditions
-  const numericalFields = [
-    { value: 'created_at_i', label: 'Created Date' },
-    { value: 'points', label: 'Points' },
-    { value: 'num_comments', label: 'Number of Comments' },
-  ];
+  
 
-  const numericalConditions = [
-    { value: '>', label: 'Greater than' },
-    { value: '>=', label: 'Greater than or equal to' },
-    { value: '=', label: 'Equal to' },
-    { value: '<', label: 'Less than' },
-    { value: '<=', label: 'Less than or equal to' },
-  ];
-
-  const tagsOptions = [ // Define the available tags
+  // Define available tags
+  const tagsOptions = [
     { value: 'story', label: 'Story' },
     { value: 'comment', label: 'Comment' },
     { value: 'poll', label: 'Poll' },
     // Add more tags as needed
   ];
 
+  // Handle the search button click
   const handleSearch = async () => {
     if (searchQuery.trim() === '') {
       alert('Please enter a search query.');
       return;
     }
 
-    // if (!selectedTags.length) {
-    //   alert('Please select at least one tag.');
-    //   return;
-    // }
-
-    // // if (!selectedField || !selectedCondition || !numericValue.trim()) {
-    // //   alert('Please specify a valid numerical filter.');
-    // //   return;
-    // }
-
     setLoading(true);
 
     try {
-      // Construct your API request based on user input and selected options.
+      //  API request based on user input and selected options.
       const tagsParam = selectedTags.map((tag) => `tags=${tag.value}`).join('&');
-      const selectedFieldParam = selectedField.value;
-      const selectedConditionParam = selectedCondition.value;
-      const numericalValueParam = numericValue;
 
-    //   const numericalFilters = `numericFilters=${selectedFieldParam}${selectedConditionParam}${numericalValueParam}`;
       const apiUrl = `http://hn.algolia.com/api/v1/search?query=${searchQuery}&${tagsParam}`;
 
-      // Simulated fetch for demonstration (replace this with an actual API call):
+      // Simulated fetch for demonstration 
       const response = await fetch(apiUrl);
       const data = await response.json();
       setSearchResults(data.hits);
@@ -76,6 +49,7 @@ function HomeScreen() {
   return (
     <div className="home-screen">
       <div className="search-bar">
+        {/* Search input */}
         <input
           type="text"
           className="search-input"
@@ -83,7 +57,8 @@ function HomeScreen() {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <Select // Select for tags
+        {/* Select for tags */}
+        <Select
           className="tag-select"
           isMulti
           options={tagsOptions}
@@ -91,52 +66,35 @@ function HomeScreen() {
           onChange={(selected) => setSelectedTags(selected)}
           placeholder="Select Tags"
         />
-        <Select // Select for numerical field
-          className="numeric-field-select"
-          value={selectedField}
-          options={numericalFields}
-          onChange={(selected) => setSelectedField(selected)}
-          placeholder="Select Numerical Field"
-        />
-        <Select // Select for numerical condition
-          className="numeric-condition-select"
-          value={selectedCondition}
-          options={numericalConditions}
-          onChange={(selected) => setSelectedCondition(selected)}
-          placeholder="Select Numerical Condition"
-        />
-        <input
-          type="number"
-          className="numeric-value-input"
-          placeholder="Numerical Value"
-          value={numericValue}
-          onChange={(e) => setNumericValue(e.target.value)}
-        />
+
+        {/* Search button */}
         <button className="search-button" onClick={handleSearch} disabled={loading}>
           {loading ? 'Searching...' : 'Search'}
         </button>
       </div>
-      {/* Display search results here */}
-      <div className="search-results-box"> {/* Box container for search results */}
-      {searchResults.length > 0 && (
-        <div className="search-results">
-          {searchResults.map((result) => (
-            <div key={result.objectID} className="result-item">
-            <Link to={`/post/${result.objectID}`}>
-          <button>Details</button>
-        </Link>
-              <p>Title: {result.title}</p>
-              <p>URL: <a href={result.url} target="_blank" rel="noopener noreferrer">{result.url}</a></p>
-              <p>Created At: {result.created_at}</p>
-              <p>Created At (Unix Timestamp): {result.created_at_i}</p>
-              <p>Number of Comments: {result.num_comments}</p>
-              <p>Points: {result.points}</p>
-              <p>Updated At: {result.updated_at}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+      {/* Display search results in a container */}
+      <div className="search-results-box">
+        {searchResults.length > 0 && (
+          <div className="search-results">
+            {searchResults.map((result) => (
+              <div key={result.objectID} className="result-item">
+                {/* Link to post detail */}
+                <Link to={`/post/${result.objectID}`}>
+                  <button>Details</button>
+                </Link>
+                {/* Display post details */}
+                <p>Title: {result.title}</p>
+                <p>URL: <a href={result.url} target="_blank" rel="noopener noreferrer">{result.url}</a></p>
+                <p>Created At: {result.created_at}</p>
+                <p>Created At (Unix Timestamp): {result.created_at_i}</p>
+                <p>Number of Comments: {result.num_comments}</p>
+                <p>Points: {result.points}</p>
+                <p>Updated At: {result.updated_at}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../App.css'
+import '../App.css';
 import { useParams } from 'react-router-dom';
 
 function Posts() {
@@ -7,8 +7,10 @@ function Posts() {
   const [postDetails, setPostDetails] = useState(null);
 
   useEffect(() => {
+    // Function to fetch post details when the component mounts
     const fetchPostDetails = async () => {
       try {
+        // Make an API request to fetch post details based on the objectID
         const response = await fetch(`http://hn.algolia.com/api/v1/items/${objectID}`);
         const data = await response.json();
         setPostDetails(data);
@@ -17,29 +19,51 @@ function Posts() {
       }
     };
 
+    // Call the fetchPostDetails function when the objectID changes
     fetchPostDetails();
   }, [objectID]);
 
-  // ...
-
-return (
+  return (
     <div className="post-detail">
-      <h2 className="post-title">{postDetails ? postDetails.title : 'Loading...'}</h2>
+      <h2 className="post-title">
+        {postDetails ? postDetails.title : 'Not available'}
+        {/* Display post title or 'Not available' if postDetails is not available */}
+      </h2>
       <div className="post-meta">
-        <p>Points: {postDetails ? postDetails.points : 'Loading...'}</p>
+        <p>
+          Points: {postDetails ? postDetails.points : 'Not available'}
+          {/* Display post points or 'Not available' if postDetails is not available */}
+        </p>
       </div>
       <h3>Comments:</h3>
       <ul className="post-comments">
-        {postDetails &&
+        {postDetails && postDetails.children ? (
           postDetails.children.map((comment) => (
             <li key={comment.id} className="comment">
               <div className="comment-content">
-                <p className="comment-author">Author: {comment.author}</p>
-                <p className="comment-text">{comment.text}</p>
+                <p className="comment-author">
+                  Author: {comment.author || 'Not available'}
+                  {/* Display comment author or 'Not available' if comment author is missing */}
+                </p>
+                <p className="comment-text">
+                  {comment.text || 'Not available'}
+                  {/* Display comment text or 'Not available' if comment text is missing */}
+                </p>
               </div>
-              <div className="comment-date">Date: {comment.created_at}</div>
+              <div className="comment-date">
+                Date: {comment.created_at || 'Not available'}
+                {/* Display comment creation date or 'Not available' if date is missing */}
+              </div>
             </li>
-          ))}
+          ))
+        ) : (
+          <li className="comment">
+            <div className="comment-content">
+              <p className="comment-text">No comments available</p>
+              {/* Display 'No comments available' if comments array is missing */}
+            </div>
+          </li>
+        )}
       </ul>
     </div>
   );
